96b1bca42f38f456836b78db3becd98cd0e77bb6
bellcliff <bell.cliff@gmail.com>
2015-12-02 15:05:00 -0500

    before db

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/user', function(req, res){
+  console.log('user post req body', req.body);
+  res.json({msg: 'ok'});
+});
+
 module.exports = router;
