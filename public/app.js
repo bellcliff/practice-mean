7374e83f0023e996fad69d1ff6ea1fe7f5a6a3f4
bellcliff <bell.cliff@gmail.com>
2015-12-02 17:16:12 -0500

    fix bug, usr and pwd still kept after logout

--- public/app.js
+++ public/app.js
@@ -2,12 +2,14 @@
 
   'use strict'
 
-  angular.module('main', ['ngCookies']).controller('LoginCtrl', function($http, $cookies){
+  angular.module('main', ['ngCookies']).controller('LoginCtrl', function($http, $cookies, $scope){
     var self = this;
     self.usr = $cookies.get('usr')
     self.login = function(usr, pwd){
       $http.post('login', {usr:usr, pwd:pwd}).success(function(usrs){
         console.log(usrs);
+        delete $scope.loginUser;
+        delete $scope.loginPwd;
         if (usrs.length > 0) {
           self.usr = usrs[0].usr;
           $cookies.put('usr', self.usr);
