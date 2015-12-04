f2b8fadabe45e821984c62f2ea66e05fa83d20ec
bellcliff <bell.cliff@gmail.com>
2015-12-04 15:44:34 -0500

    delete acc supp

--- public/app.js
+++ public/app.js
@@ -11,8 +11,14 @@
     };
 
     self.logout = function(){
-        self.usr = undefined;
+      self.usr = undefined;
       $http.get('logout').success(function(){
+      })
+    }
+
+    self.del = function(){
+      self.usr = undefined;
+      $http.delete('del').success(function(){
       })
     }
 
@@ -21,6 +27,13 @@
         self.usr = info.usr;
       }).catch(function(){});
     }
+
+    self.sign = function(usr, pwd){
+      $http.post('sign', {usr: usr, pwd: pwd}).success(function(){
+         self.usr = usr
+      })
+    }
+
     self.check();
   });
 })()
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var mc = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var dbCon = mc.connect('mongodb://localhost:27017/test');
+var Q = require('q');
 
 router.get('/check', function(req, res, next){
   console.log(req.cookies)
@@ -14,16 +17,18 @@ router.get('/check', function(req, res, next){
 
 router.get('/logout', function(req, res){
   if (req.session.usr)
-    req.session.destroy()
-  res.json({})
+    req.session.destroy(function(){
+      res.json({})
+    })
+  else res.json({})
 })
 
 router.post('/login', function(req, res, next){
   if (req.session.usr) {
-    res.json({msg: 'ok'})
+    res.json({msg: 'session'})
     return
   }
-  mc.connect('mongodb://localhost:27017/test').then(function(db){
+  dbCon.then(function(db){
     return db.collection('user').find(req.body).toArray()
   }).then(function(usrs){
     if (!usrs || usrs.length == 0)
@@ -34,17 +39,48 @@ router.post('/login', function(req, res, next){
         next(err);
         return;
       }
-      res.json({msg: 'ok'})
+      res.json({msg: 'db'})
     });
   }).catch(function(err){next(err)});
 });
 
-router.post('/user', function(req, res, next){
+router.post('/sign', function(req, res, next){
   console.log('user post req body', req.body);
-  mc.connect('mongodb://localhost:27017/test').then(function(db){
-    return db.collection('user').update(req.body, req.body, {upsert: true})
-  }).then(res.json.bind(res)).catch(function(err){next(err)});
+  mc.connect('mongodb://localhost:27017/test')
+    .then(function(db){
+      return db.collection('user').insertOne(req.body)
+    })
+    .then(function(info){
+       console.log('after save', info)
+       req.session.usr = req.body
+       return Q.nfcall(req.session.save.bind(req.session))
+               .then(function(){return {}})
+    })
+    .then(res.json.bind(res))
+    .catch(function(err){next(err)});
 });
+
+router.delete('/del', function(req, res, next){
+  if (req.session.usr) {
+    dbCon.then(function(db){
+        return db.collection('user').deleteOne({_id: new ObjectId(req.session.usr._id)})
+      })
+      .then(function(info){
+         console.log('del result', info)
+         return Q.nfcall(req.session.destroy.bind(req.session))
+                 .then(function(){
+         	   return dbCon.then(function(db){return db.collection('user').find().toArray()})
+                 })
+      })
+      .then(function(info){
+        res.json(info)
+        res.end()
+      })
+      .catch(function(err){next(err)});
+  }else{
+    res.json({msg: 'need login'});
+  }
+})
 
 router.get('/user', function(req, res, next){
   mc.connect('mongodb://localhost:27017/test').then(function(db){
